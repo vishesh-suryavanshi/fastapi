eb036cc1c0eb3af134736af4e97542dde559fb9b
Vishesh Suryavanshi <vishesh.s@consultadd.com>
2023-12-27 14:00:32 +0530

    db creation flow updated

--- data_script.py
+++ data_script.py
@@ -5,8 +5,6 @@ from pathlib import Path
 from datetime import datetime
 from dotenv import load_dotenv
 
-import models
-from database import engine
 
 
 def load_file_data():
@@ -19,7 +17,6 @@ def load_file_data():
 
         start_time = datetime.now()
 
-        models.Base.metadata.create_all(bind=engine)
         folder_path = 'data/wx_data'
         file_list = os.listdir(folder_path)
 
@@ -40,31 +37,32 @@ def load_file_data():
         select_id_of_station = f'SELECT id from station where name=%s'
 
         for file_name in file_list:
-            file_path = os.path.join(folder_path, file_name)
-            all_records = []
-            station_name = Path(file_path).stem
-            cursor.execute(insert_into_station, (station_name,))
-            cursor.execute(select_id_of_station, (station_name,))
-            station = cursor.fetchone()
-            station_id = station[0]
-            with open(file_path, 'r') as file:
-                for line in file:
-                    data = line.strip().split('\t')
-                    if '-9999' in data:
-                        continue
-                    else:
-                        temp_line = []
-                        for idx, cell in enumerate(data):
-                            if idx == 0:
-                                temp_line.append(datetime.strptime(str(cell), '%Y%m%d').date())
-                            else:
-                                temp_line.append(int(cell.strip()))
-                        temp_line.append(station_id)
-                        all_records.append(temp_line)
-            for record in all_records:
-                cursor.execute(insert_into_record, record)
-            end_time = datetime.now()
-            logging.info(f'Start time {start_time}, End time {end_time}, Inserted Records {len(all_records)}')
+            if file_name.endswith('.txt'):
+                file_path = os.path.join(folder_path, file_name)
+                all_records = []
+                station_name = Path(file_path).stem
+                cursor.execute(insert_into_station, (station_name,))
+                cursor.execute(select_id_of_station, (station_name,))
+                station = cursor.fetchone()
+                station_id = station[0]
+                with open(file_path, 'r') as file:
+                    for line in file:
+                        data = line.strip().split('\t')
+                        if '-9999' in data:
+                            continue
+                        else:
+                            temp_line = []
+                            for idx, cell in enumerate(data):
+                                if idx == 0:
+                                    temp_line.append(datetime.strptime(str(cell), '%Y%m%d').date())
+                                else:
+                                    temp_line.append(int(cell.strip()))
+                            temp_line.append(station_id)
+                            all_records.append(temp_line)
+                for record in all_records:
+                    cursor.execute(insert_into_record, record)
+                end_time = datetime.now()
+                logging.info(f'Start time {start_time}, End time {end_time}, Inserted Records {len(all_records)}')
         connection.commit()
         cursor.close()
         connection.close()
@@ -111,5 +109,3 @@ def calculate_stats_data():
         print(str(e))
 
 
-load_file_data()
-calculate_stats_data()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Depends, Query
+
 import models
+from data_script import calculate_stats_data, load_file_data
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
 from typing import Annotated, Optional
@@ -18,22 +20,39 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def on_startup():
+    try:
+        db = next(get_db())
+        models.Base.metadata.create_all(bind=engine)
+        if not (db.query(models.Record).count() or db.query(models.Station).count()):
+            load_file_data()
+        if not db.query(models.Stats).count():
+            calculate_stats_data()
+    except Exception as e:
+        print(f"Error during data loading: {str(e)}")
+
+
+@app.on_event("startup")
+async def startup():
+    on_startup()
+
+
 @app.get('/api/weather')
 async def get_weather(db: db_dependency, station_id: Optional[int] = None, date: Optional[str] = None,
                       page: int = Query(0), limit: int = Query(10)):
     """
-     Get weather data from the specified station and date.
+       Get weather data from the specified station and date.
 
-        Parameters:
-        - db (db_dependency): The database dependency to retrieve the weather data.
-        - station_id (Optional[int]): The ID of the weather's station. If it is None, data for all stations will be retrieved.
-        - date (Optional[str]): The date for which weather data is requested. If None, data for all dates will be retrieved.
-        - page (int): The page number for paginated results (default is 0).
-        - limit (int): The number of items to return per page (default is 10).
+       Args:
+       - db:The database dependency to retrieve data.
+       - station_id:The id of the station.
+       - date:The date for which data is requested.
+       - page: The page number for paginated results .
+       - limit: The number of items to return per page .
 
-        Returns:
-        - Weather data for the specified station and date, paginated according to the provided page and limit.
-        """
+       Returns:
+       - JSON data of Weather for the specified station and date (if any).
+       """
     try:
         queryset = db.query(models.Record)
         if station_id:
@@ -54,18 +73,18 @@ async def get_weather(db: db_dependency, station_id: Optional[int] = None, date:
 async def get_stats(db: db_dependency, station_id: Optional[int] = None, year: Optional[int] = None,
                     page: int = Query(0), limit: int = Query(10)):
     """
-        Get statistical data for a weather station and a specific year.
+         Get statistical data for a weather station and a specific year.
 
-        Parameters:
-        - db (db_dependency): The database dependency to retrieve statistical data.
-        - station_id (Optional[int]): The ID of the weather station. If None, data for all stations will be retrieved.
-        - year (Optional[int]): The year for which statistical data is requested. If it is None, data for all years will be retrieved.
-        - page (int): The page number for paginated results (default is 0).
-        - limit (int): The number of items to return per page (default is 10).
+        Args :
+        - db : The database dependency to retrieve statistical data.
+        - station_id: The ID of the weather station.
+        - year  The year for which statistical data is requested.
+        - page: The page number for paginated results.
+        - limit: The number of items to return per page.
 
         Returns:
-        - Statistical data for the specified weather station and year, paginated according to the provided page and limit.
-        """
+        - Statistical data for the specified weather station and year.
+    """
     try:
         queryset = db.query(models.Stats)
         if station_id:
